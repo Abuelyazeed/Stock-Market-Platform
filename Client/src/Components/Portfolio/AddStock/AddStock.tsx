6db8ec2c5cb201fbcc69bcf776bd5dec6d2c5857
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2025-01-02 10:39:09 +0200

    added tailwind styling

--- Client/src/Components/Portfolio/AddStock/AddStock.tsx
+++ Client/src/Components/Portfolio/AddStock/AddStock.tsx
@@ -6,10 +6,17 @@ interface Props {
 }
 const AddStock = ({ onStockAdd, symbol }: Props) => {
   return (
-    <form onSubmit={onStockAdd}>
-      <input readOnly={true} hidden={true} type="text" value={symbol} />
-      <button type="submit">Add</button>
-    </form>
+    <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+      <form onSubmit={onStockAdd}>
+        <input readOnly={true} hidden={true} value={symbol} />
+        <button
+          type="submit"
+          className="p-2 px-8 text-white bg-darkBlue rounded-lg hover:opacity-70 focus:outline-none"
+        >
+          Add
+        </button>
+      </form>
+    </div>
   );
 };
 export default AddStock;
