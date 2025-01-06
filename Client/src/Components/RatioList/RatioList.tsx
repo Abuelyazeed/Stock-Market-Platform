4ff004f75520f4860f6f6cefe3c22fe328eba064
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2025-01-06 10:40:31 +0200

    created the table component

---
+++ Client/src/Components/RatioList/RatioList.tsx
@@ -0,0 +1,9 @@
+import { TestDataCompany } from '../Table/testData';
+
+type Props = {};
+
+const data = TestDataCompany[0];
+const RatioList = (props: Props) => {
+  return <div>RatioList</div>;
+};
+export default RatioList;
