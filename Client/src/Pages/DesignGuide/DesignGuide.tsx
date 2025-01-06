c0861c8114925ed48aab1134643c46a7e7a2c5ec
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2025-01-06 10:45:35 +0200

    created ratiolist component

--- Client/src/Pages/DesignGuide/DesignGuide.tsx
+++ Client/src/Pages/DesignGuide/DesignGuide.tsx
@@ -1,3 +1,4 @@
+import RatioList from '../../Components/RatioList/RatioList';
 import Table from '../../Components/Table/Table';
 
 type Props = {};
@@ -8,6 +9,7 @@ const DesignGuide = (props: Props) => {
         Design guide- This is the design guide for Fin Shark. These are reuable
         components of the app with brief instructions on how to use them.
       </h1>
+      <RatioList />
       <Table />
       <h3>
         Table - Table takes in a configuration object and company data as
